07c849b6055c25b6634c536197a466e9c33be82d
Gregfanyan <grigorfanyan@gmail.com>
2021-06-23 11:44:06 +0200

    feat: added like button

--- components/PostItem/index.js
+++ components/PostItem/index.js
@@ -1,13 +1,26 @@
-import { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Card from "../Card";
+import styles from "./PostItem.module.css";
 
 function PostItem({ post }) {
+  const [liked, setLiked] = useState(false);
+
+  const handleClick = () => {
+    setLiked(!liked);
+  };
+
   return (
     <Card>
-      <FontAwesomeIcon icon="heart" size="3x" transform="shrink-4 right-130" color='grey' />
+      <FontAwesomeIcon
+        icon="heart"
+        size="3x"
+        transform="shrink-4 right-130"
+        color={liked ? "grey" : "black"}
+        onClick={handleClick}
+      />
       <Link href={`/${post.id}`} passHref>
         <h1>{post.title} &rarr;</h1>
       </Link>
